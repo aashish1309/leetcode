3ea8ee7fd409063eaa27eaa340f3df62e96554fb
Aashish <125014044+aashish1309@users.noreply.github.com>
2024-02-24 08:04:31 +0530

    Time: 3 ms (60.82%) | Memory: 14.2 MB (15.71%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,16 +9,16 @@ public:
 
         while (!q.empty()) {
             int size = q.size();
-            int val = 0;
             for (int i = 0; i < size; i++) {
                 TreeNode* node = q.front();
                 q.pop();
 
-                val = node->val; 
+                if (i == size - 1) // If it's the rightmost node at this level
+                    result.push_back(node->val);
+
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
             }
-            result.push_back(val); 
         }
         return result;
     }
