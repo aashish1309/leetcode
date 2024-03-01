e1782c28e6735f577a19258329b097be43094500
Aashish <125014044+aashish1309@users.noreply.github.com>
2024-03-01 08:31:28 +0530

    Time: 534 ms (74.95%) | Memory: 298.3 MB (52.51%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -8,22 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        if(!head || !head->next){
-            return NULL;
+        
+        ios::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        if(head == NULL || head->next == NULL) return NULL;
+        
+        ListNode *slow = head ;
+        ListNode *fast = head ;
+        ListNode *prev = NULL ;
+        
+        while(fast!=NULL and fast->next!=NULL)
+        {
+            fast = fast->next->next;
+            prev = slow;
+            slow = slow->next;
         }
-        ListNode* slow=head;
-        ListNode* fast=head;
-        fast=fast->next->next;
-        while(fast && fast->next){
-            fast=fast->next->next;
-            slow=slow->next;
-        }
-        ListNode* del=slow->next;
-        slow->next=slow->next->next;
-        delete del;
+        prev->next = slow->next;
+        slow->next = NULL;
+        delete slow;
+        
         return head;
-    }
+        
+        
+    }   
 };
