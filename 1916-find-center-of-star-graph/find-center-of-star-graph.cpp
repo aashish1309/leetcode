e8eea6c65119de7afa5116d82366d3045cb7c362
Aashish <125014044+aashish1309@users.noreply.github.com>
2024-03-11 12:32:21 +0530

    Time: 120 ms (96.16%) | Memory: 70.8 MB (72.13%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,40 +1,13 @@
-int init = [] {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	ofstream out("user.out");
-	cout.rdbuf(out.rdbuf());
-	for (string line; getline(cin, line); cout << '\n') {
-		istringstream ss(line);
-		char ch;
-		int a, b, c, d;
-		ss >> ch >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d;
-		if (a == c || a == d) cout << a;
-		else cout << b;
-	}
-	exit(0);
-	return 0;
-}();
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-               int n = edges.size() + 1;
-        std::vector<std::vector<int>> adj(n + 1, std::vector<int>());
-
-        for (int i = 0; i < edges.size(); i++) {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-        }
-
-
-
-        for (int i = 1; i <= n; i++) {
-           if(adj[i].size()>1)
-           return i;
-        }
-        return 0;
-
-        
-
-
+        int center;
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
+            center = edges[0][0];
+        else if(edges[0][1]==edges[1][0] || edges[0][1]==edges[1][1])
+            center = edges[0][1];
+        else
+            return -1;
+        return center;
     }
 };
